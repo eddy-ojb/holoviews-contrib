2cbec674708eb06b55248ad83129cae0fd4b68c2
jlstevens <jlstevens@ed.ac.uk>
2016-09-27 21:12:35 +0100

    Added scripts subdirectory and template.py

---
+++ scripts/template.py
@@ -0,0 +1,14 @@
+"""
+Name:        Template
+HoloViews:   1.6.2
+Author:      Author
+Python:      2.7
+
+Please fill in the three fields above as appropriate and replace this
+text with a short description of what this script demonstrates. If the
+script requires any dependencies (including plotting or data backends),
+please show the appropriate pip or conda install command as follows:
+
+   `conda install bokeh xarray`
+
+"""
